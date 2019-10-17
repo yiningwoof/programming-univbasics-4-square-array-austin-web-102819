e19e4b52378df2b1847efaad91792b1c9ad787c9
Yining Wang <yining.wang@utexas.edu>
2019-10-17 21:41:54 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0
+  while counter < array.length do
+    new_array.push((array[counter])**2)
+    counter += 1
+  end
+  new_array
 end
